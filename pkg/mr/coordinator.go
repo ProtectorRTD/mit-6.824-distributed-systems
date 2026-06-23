31937258932f7ef5c0c58c9511d56e6cda0e4867
ProtectorRTD <72699799+ProtectorRTD@users.noreply.github.com>
2026-06-23 23:33:45 +0200

    Master +- finish, without polish, and currently bad state machine, can be in future redesign

--- pkg/mr/coordinator.go
+++ pkg/mr/coordinator.go
@@ -10,40 +10,66 @@ import (
 	"net/rpc"
 	"os"
 	"sync"
+	"time"
 )
 
 //TODO read about the wait in MapReduce
 
 type Coordinator struct {
-	mutex        sync.Mutex
-	NReduce      int
-	NMap         int
-	Files        []string
-	Tasks        []Task
-	pendingTask  map[string]Task
-	RemainingMap uint8
+	mutex           sync.Mutex
+	NReduce         int
+	NMap            int
+	Files           []string
+	Tasks           []Task
+	pendingTask     map[string]Task
+	startedAt       map[string]time.Time
+	RemainingMap    uint8
+	RemainingReduce uint8
+	reduceGenerated bool
 }
 
-//http://nil.csail.mit.edu/6.824/2020/labs/lab-mr.html
-//нельзя давать редьюс таск, пока не сделан маппинг
-//есть кейс, когда воркер сделает таск, но маппинг ещё не придет,
-//второй момент, мастер может дать таску, но если за 10 сек не пришел ответ
-//то мы возвращаем таску в пул и все (и считаем что эта таска не была совершена)
-
+// стейты плохо менеджерится тасков, надо исправить
+// http://nil.csail.mit.edu/6.824/2020/labs/lab-mr.html
 func (c *Coordinator) RequestTask(args *RequestTask, reply *Task) error {
 	c.mutex.Lock()
 	//invariant, что нету мапТасков + все сделаны
-	if c.RemainingMap <= 0 {
+	// (бесконечно может быть), попросит новую, создат редьюс
+	if c.RemainingReduce <= 0 && c.RemainingMap <= 0 {
+		*reply = *c.generateExitTask()
+		c.Done()
+		return nil
+	}
+	if c.RemainingMap <= 0 && c.reduceGenerated == false {
+		c.reduceGenerated = true
 		c.generateReduceTask()
 	}
 
-	//TODO если реквест, ассинхронный таймер на 10 сек, что все ок, если прошло,
-	//то возвращаем таску в очередь тип воркер мертв
+	if len(c.Tasks) == 0 {
+		c.mutex.Unlock()
+		return nil
+	}
+
 	head := c.Tasks[0]
-	*reply = head
 	c.Tasks = c.Tasks[1:]
+
+	*reply = head
+
+	taskId := head.TaskId
+	c.pendingTask[taskId] = head
+	c.startedAt[taskId] = time.Now()
 	c.mutex.Unlock()
 	return nil
+}
+
+func (c *Coordinator) generateExitTask() *Task {
+	task := &Task{
+		TaskId:    newID(),
+		TaskState: COMPLETED,
+		TaskType:  EXIT,
+		NReduce:   uint8(c.NReduce),
+		NMap:      uint8(c.NMap),
+	}
+	return task
 }
 
 func (c *Coordinator) generateReduceTask() {
@@ -61,15 +87,24 @@ func (c *Coordinator) generateReduceTask() {
 
 func (c *Coordinator) reportDone(result *ResultOfTask) error {
 	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	if !result.Success {
 		return fmt.Errorf("worker reported failure for task %s", result.TaskId)
 	}
 	taskId := result.TaskId
-	if c.pendingTask[taskId].TaskType == 2 {
-		c.RemainingMap--
+	task, ok := c.pendingTask[taskId]
+	if !ok {
+		// taskId не существует
+	} else {
+		if c.pendingTask[taskId].TaskType == MAP {
+
+			c.RemainingMap--
+		}
+		if c.pendingTask[taskId].TaskType == REDUCE {
+			c.RemainingReduce--
+		}
+		delete(c.pendingTask, taskId)
 	}
-	delete(c.pendingTask, taskId)
-	c.mutex.Unlock()
 	return nil
 }
 
@@ -94,17 +129,39 @@ func (c *Coordinator) Done() bool {
 	return false
 }
 
+func (c *Coordinator) sheduler() {
+	for {
+		time.Sleep(time.Second)
+		c.mutex.Lock()
+		for taskId, task := range c.pendingTask {
+			started := c.startedAt[taskId]
+
+			if time.Since(started) > 10*time.Second {
+				task.TaskState = IDLE
+
+				c.Tasks = append(c.Tasks, task)
+
+				delete(c.pendingTask, taskId)
+				delete(c.startedAt, taskId)
+			}
+		}
+		c.mutex.Unlock()
+	}
+}
+
 // main/mrcoordinator.go calls this function.
 func MakeCoordinator(fileNames []string, nReduce int) *Coordinator {
 	mapAmount := len(fileNames)
 	c := &Coordinator{
-		mutex:        sync.Mutex{},
-		NReduce:      nReduce,
-		NMap:         mapAmount,
-		Files:        fileNames,
-		Tasks:        make([]Task, 0),
-		pendingTask:  make(map[string]Task),
-		RemainingMap: uint8(mapAmount),
+		mutex:           sync.Mutex{},
+		NReduce:         nReduce,
+		NMap:            mapAmount,
+		Files:           fileNames,
+		Tasks:           make([]Task, 0),
+		pendingTask:     make(map[string]Task),
+		RemainingMap:    uint8(mapAmount),
+		RemainingReduce: uint8(nReduce),
+		reduceGenerated: false,
 	}
 	for i := 0; i < mapAmount; i++ {
 		task := Task{
@@ -118,6 +175,7 @@ func MakeCoordinator(fileNames []string, nReduce int) *Coordinator {
 		c.Tasks = append(c.Tasks, task)
 	}
 	c.server()
+	go c.sheduler()
 	return c
 }
 
